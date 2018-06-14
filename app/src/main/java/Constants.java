714b148f4932b4e7b96c6b7660fc86b45c89de20
davidlutta <davidlutta2010@gmail.com>
2018-06-14 09:34:40 +0300

    create project

---
+++ app/src/main/java/Constants.java
@@ -0,0 +1,6 @@
+import com.restaurants.agnes.myrestaurants.BuildConfig;
+
+public class Constants {
+    public static final String API_KEY = BuildConfig.yelpApiKey;
+    public static final String BASE_URL = ;
+}
